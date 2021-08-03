6585eb6a35746dd2de223b1780cb80fb3198b8ec
Gassacu <88395597+Gassacu@users.noreply.github.com>
2021-08-03 11:37:31 -0500

    Add files via upload

---
+++ lista.py
@@ -0,0 +1,11 @@
+lista=["hola",7,3.5,True,9,"mundo",False,4.5]
+print(lista)
+print(lista[1])
+print(lista[0:3])
+print(lista[5:8])
+print(lista[3:5])
+lista.append("curso")
+print(lista)
+lista[3]=lista[7]
+print(lista)
+print(lista[0:9:3])
